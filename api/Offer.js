9fb2a7d3443a752fecaa11d23ecfb36bd4e42999
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2015-04-11 21:05:01 -0600

    fix(offers): 404
    closes #19

--- api/Offer.js
+++ api/Offer.js
@@ -3,15 +3,12 @@ var OfferModel = require('../models/Offer');
 var Offer = module.exports = {};
 
 Offer.show = function * (next) {
-	var user = this.user;
-	var offer = yield OfferModel.findOneAsync({_id: this.params.id}).then(function (offer) {
-		return offer.show(user);
-	});
+	var offer = yield OfferModel.findOneAsync({_id: this.params.id});
 	if (!offer) {
 		this.status = 404;
 	} else {
 		this.status = 200;
-		this.body = offer;
+		this.body = yield offer.show(this.user);
 	}
 	yield next;
 };
