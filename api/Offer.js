e0658b1dc08049799123c0655c919b845e92b030
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2015-05-01 14:50:10 -0600

    fix(offer api): 404 on GET draft offer as non-admin

--- api/Offer.js
+++ api/Offer.js
@@ -4,11 +4,12 @@ var Offer = module.exports = {};
 
 Offer.show = function * (next) {
 	var offer = yield OfferModel.findOneAsync({_id: this.params.id});
-	if (!offer) {
-		this.status = 404;
-	} else {
+	if (offer) {
 		this.body = yield offer.show(this.user);
-		this.status = 200;
+		this.status = this.body ? 200 : 404;
+	} else {
+		// Offer not found
+		this.status = 404;
 	}
 	yield next;
 };
