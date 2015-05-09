b7e86a9c6f7bce1c7c5974959ad120027c773ab1
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2015-05-09 16:38:53 -0600

    refactor(config): use nconf for secret in auth.js. Provides temp secret for development, env var secret for production.
    - closes #23, #2

--- config/config.js
+++ config/config.js
@@ -3,6 +3,10 @@ var nconf = module.exports = require('nconf');
 //nconf.argv().env();
 
 if (nconf.get('NODE_ENV') === 'production') {
+	// jwtSecret must be set at command line or environment variable in production and staging
+	if (nconf.get('jwtSecret') === undefined) {
+		throw new Error('jwtSecret must be provided as a command line argument or environment variable');
+	}
 } else if (nconf.get('NODE_ENV') === 'staging') {
 } else {
 	// Development
