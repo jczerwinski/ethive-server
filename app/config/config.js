bbbe7bc3a8a4820e190b5c7e2f8a3a122cfa6be0
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2015-05-14 09:04:31 -0600

    feat(production config): first crack at modulus production config

--- app/config/config.js
+++ app/config/config.js
@@ -3,10 +3,11 @@ var nconf = module.exports = require('nconf');
 //nconf.argv().env();
 
 if (nconf.get('NODE_ENV') === 'production') {
-	// jwtSecret must be set at command line or environment variable in production and staging
-	if (nconf.get('jwtSecret') === undefined) {
+	nconf.add('literal', require('./production.json'));
+	// jwtSecret must be set at command line or environment variable in production and staging, at least once there are more people working on it
+	/*if (nconf.get('jwtSecret') === undefined) {
 		throw new Error('jwtSecret must be provided as a command line argument or environment variable');
-	}
+	}*/
 } else if (nconf.get('NODE_ENV') === 'staging') {
 } else {
 	// Development
