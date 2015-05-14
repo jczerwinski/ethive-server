24b006e1873dde63debef4b8887c5acb09ee3c80
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2015-05-14 09:17:48 -0600

    Debugging production mongo connection

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,5 +1,6 @@
 // Database config
 var mongoose = require('mongoose');
 var servers = require('./servers');
+console.log(servers.mongo)
 mongoose.connect(servers.mongo);
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
