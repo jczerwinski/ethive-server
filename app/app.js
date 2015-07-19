80d3669a6067d4c165a0923db872337acce72a2a
Jamie Czerwinski <jamie.czerwinski@gmail.com>
2015-07-19 15:19:40 -0600

    remove appdynamics

--- app/app.js
+++ app/app.js
@@ -1,13 +1,4 @@
 'use strict';
-require("appdynamics").profile({
-    controllerHostName: 'paid138.saas.appdynamics.com',
-    controllerPort: 443, // If SSL, be sure to enable the next line     controllerSslEnabled: true // Optional - use if connecting to controller via SSL
-    accountName: 'ethive', // Required for a controller running in multi-tenant mode
-    accountAccessKey: '281v5z9q8k7j', // Required for a controller running in multi-tenant mode
-    applicationName: 'ethive',
-    tierName: 'api',
-    nodeName: 'process' // The controller will automatically append the node name with a unique number
-});
 var http = require('http');
 var config = require('./config/config');
 var servers = require('./lib/servers');
